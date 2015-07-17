6c919bde49179565d6ef23880b71d48d435971e9
mgalpy@yahoo.com <mgalpy@yahoo.com>
2015-07-16 17:27:56 -0700

    Step 5: Add AngularJS Authentication Service (Factory)

---
+++ AngularJSAuthentication.API/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+
+namespace AngularJSAuthentication.API
+{
+    public static class Helper
+    {
+        public static void Write(string tag, IPrincipal principal)
+        {
+
+            Debug.WriteLine("-------- " + tag + " --------");
+            if (principal == null || 
+                principal.Identity == null ||
+                !principal.Identity.IsAuthenticated)
+            {
+                Debug.WriteLine("anonymous user");
+            } else {
+                Debug.WriteLine("User: " + principal.Identity.Name);
+            }
+            Debug.WriteLine("\n");
+        }
+    }
+}
